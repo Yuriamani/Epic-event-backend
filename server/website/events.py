82aedb334140a83bd9d377d34a28d200916b8721
Yuriamani25 <yuriamani7@gmail.com>
2024-08-12 08:34:54 +0300

    commits

--- server/website/events.py
+++ server/website/events.py
@@ -6,20 +6,10 @@ from flask_restful import Api, Resource
 events = Blueprint('events', __name__)
 api = Api(events)
 
-class Events(Resource):
+class EventList(Resource):
     def get(self):
         events = Event.query.all()
         return [event.to_dict() for event in events], 200
-
-    def post(self):
-        data = request.json
-        required_fields = ['name', 'image', 'datetime', 'location', 'capacity', 'description']
-        if not validate_request_data(data, required_fields):
-            return handle_error('Missing required fields', 400)
-        event = Event(name=data['name'],image=data['image'], datetime=data['datetime'], location=data['location'], capacity=data['capacity'], description=data['description'])
-        db.session.add(event)
-        db.session.commit()
-        return event.to_dict(), 201
     
     def patch(self):
         data = request.json
@@ -46,10 +36,29 @@ class Events(Resource):
 
         db.session.commit()
         return event.to_dict(), 200
-    
-    def delete(self):
+
+    def post(self):
         data = request.json
-        id = data.get('id')
+        required_fields = ['name', 'image', 'datetime', 'location', 'capacity', 'description']
+        if not validate_request_data(data, required_fields):
+            return handle_error('Missing required fields', 400)
+        event = Event(name=data['name'],image=data['image'], datetime=data['datetime'], location=data['location'], capacity=data['capacity'], description=data['description'])
+        db.session.add(event)
+        db.session.commit()
+        return event.to_dict(), 201
+    
+    
+class Event(Resource):
+    def get(self, id):
+        if id is None:
+            return {'error': 'Missing event ID'}, 400
+
+        event = Event.query.get(id)
+        if event is None:
+            return {'error': 'Event not found'}, 404
+        return event.to_dict(), 201
+
+    def delete(self, id):
         if id is None:
             return {'error': 'Missing event ID'}, 400
 
@@ -59,6 +68,7 @@ class Events(Resource):
 
         db.session.delete(event)
         db.session.commit()
-        return {'message': 'Event deleted successfully'}, 200
+        return {'message': 'Event deleted successfully'}, 200    
 
-api.add_resource(Events, '/events') 
+api.add_resource(Event, "/events/<int:id>")
+api.add_resource(EventList, "/events")
